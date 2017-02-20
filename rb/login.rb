e3d818f7652f3dc88787b67664112eeddf387416
tessai9 <lon1ago9@yahoo.co.jp>
2017-02-20 23:38:16 +0900

    HTTP server for Test. page1.html modified

--- rb/login.rb
+++ rb/login.rb
@@ -1,1 +1,18 @@
 # ログイン処理
+#require "sqlite3"
+require "net/http"
+require "cgi"
+
+#database = SQLite3::DataBase.new('data.rb')
+
+request = CGI.new
+uid = request['id']
+upass = request['pass']
+
+if uid == "1" && upass == "1"
+  #request.out("text/plain"){"mypage.html"}
+  puts "mypage.html"
+else
+  #request.out("text/plain"){"mypage.html"}
+  puts "mypage.html"
+end
---
+++ rb/server.rb
@@ -0,0 +1,10 @@
+require "webrick"
+
+server = WEBrick::HTTPServer.new({
+    :DocumentRoot => '../',
+    :BindAddress => '127.0.0.1',
+    :Port => '8000'
+  })
+
+trap("INT"){ server.shutdown }
+server.start

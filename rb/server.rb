eca7a89ad5d3c2b9344fa87c36eb1f44a4c82d72
tessai9 <lon1ago9@yahoo.co.jp>
2017-03-01 22:08:55 +0900

    ちょっと修正

--- rb/server.rb
+++ rb/server.rb
@@ -1,6 +1,6 @@
 require "webrick"
 #require "uri"
-require "./switch.rb"
+#require "./switch.rb"
 
 class ServletAction < WEBrick::HTTPServlet::AbstractServlet
     def do_POST (req, res)
@@ -13,9 +13,11 @@ server = WEBrick::HTTPServer.new({
     :BindAddress => '127.0.0.1',
     :Port => '8000'
   })
+=begin
 server.mount_proc('/rb'){|req,res|
   res.content_type = "text/plain"
   res.body = req.body
 }
+=end
 trap("INT"){ server.shutdown }
 server.start
